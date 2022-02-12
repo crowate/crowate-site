1c79aa14813f19f34a719beb39d33732022e5dd6
Forehand, Dayton <daytonforehand@live.com>
2022-02-12 00:36:33 -0600

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as About } from './profile/Profile';
+export {default as Home } from './home/Home';
+export {default as Navbar } from './navbar/Navbar';
+export {default as Header } from './header/Header';
