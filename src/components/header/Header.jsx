9c306eba954cb43f02d0d01ff1253f74fe08eb83
Forehand, Dayton <daytonforehand@my.unt.edu>
2022-02-17 09:04:15 -0600

    Formatting fix + form link added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="header__text_container">
           <h1 className='header__text header__text-title'>Crowate</h1>
           <h2 className='header__text header__text-subtitle'>A platform by artists, for artists</h2>
-          <button className='header__text header__text-button'>Join our Mailing List</button>
+          <a className='header__text header__text-button' href='https://docs.google.com/forms/d/e/1FAIpQLSdyUrLOQe4IbghI9A3f_0IJXhh4b8hb5u_9fF_3SYaSCf7AyQ/viewform'>Join our Mailing List</a>
         </div>
     </div>
   )
