6c24031147019cdd40cd9dbb95b539c558f04c52
julian-garcia-hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-11 09:35:48 -0500

    changed home page render to app again

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './pages/home/Home'
+import Post from './pages/post/Post'
 
 import Update from './components/Profile/ProfileUpdate'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,   
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
