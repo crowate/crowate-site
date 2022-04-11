6fecd6c362a1fdbd074ae61ee6589842bce5eb83
julian-garcia-hernandez <julian.garcia.hernandez11@gmail.com>
2022-04-11 09:37:31 -0500

    components: searchbar & navbar, subject to improve

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Navbar } from './navbar/Navbar';
 export { default as Gallery } from './gallery/Gallery';
 export { default as UploadForm } from './upload_form/UploadForm'
 export { default as Searchbar } from './searchbar/Searchbar'
+export { default as Modal } from './modal/Modal'
+export { default as UpdateProfile } from './update_profile/UpdateProfile'
